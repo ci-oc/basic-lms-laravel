191196a117d18c2bb8b4f92f60ccc8a3a8ba9fe2
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2017-08-10 21:55:26 +0200

    Test case UI - Issue

--- public/js/problem_js/script.js
+++ public/js/problem_js/script.js
@@ -1,35 +1,34 @@
 function addRow(tableID) {
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	if(rowCount < 50){							// limit the user from creating fields more than your limits
-		var row = table.insertRow(rowCount);
-		var colCount = table.rows[0].cells.length;
-		for(var i=0; i<colCount; i++) {
-			var newcell = row.insertCell(i);
-			newcell.innerHTML = table.rows[0].cells[i].innerHTML;
-		}
-	}else{
-		 alert("Maximum number if test cases is 50.");
-			   
-	}
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    if (rowCount < 50) {							// limit the user from creating fields more than your limits
+        var row = table.insertRow(rowCount);
+        var colCount = table.rows[0].cells.length;
+        for (var i = 0; i < colCount; i++) {
+            var newcell = row.insertCell(i);
+            newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+        }
+    } else {
+        alert("Maximum number if test cases is 50.");
+
+    }
 }
 
 function deleteRow(tableID) {
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	for(var i=0; i<rowCount; i++) {
-		var row = table.rows[i];
-		var chkbox = row.cells[0].childNodes[0];
-		if(null != chkbox && true == chkbox.checked) {
-			if(rowCount <= 3) { 						// limit the user from removing all the fields
-				alert("Minimum number of test cases is 3, Test cases +3 will be deleted");
-				break;
-			}
-			else{
-				table.deleteRow(i);
-				rowCount--;
-				i--;
-			}
-		}
-	}
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    if (rowCount < 3) { 						// limit the user from removing all the fields
+        alert("Minimum number of test cases is 3, Test cases +3 will be deleted");
+    } else {
+        for (var i = 0; i < rowCount; i++) {
+            var row = table.rows[i];
+            var chkbox = row.cells[0].childNodes[0];
+            if (null != chkbox && true == chkbox.checked) {
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+
+            }
+        }
+    }
 }

e04cec276814d8d5db5c69794f360aee83853527
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2017-08-20 17:18:10 +0200

    ProfileController converted to resource

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,22 +3,85 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Intervation\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
-    //
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return view('user_profile');
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-    	if ($request->hasFile('image')) {
-    		Image::make($request->file('image'))->save(public_path('images/avatar/' . Auth()->email .' _profile'));
+        if ($request->hasFile('image')) {
+            Image::make($request->file('image'))->save(public_path('images/avatar/' . Auth()->email . ' _profile'));
 
-    	}
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }

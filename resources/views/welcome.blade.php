6bede85b2c6ad3c30e6cc98317b7e39dd6dffedc
MinaMofreh <minamofreh35@gmail.com>
2017-08-24 02:30:08 +0200

    :sunrise: Welcome and login animated done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,284 @@
 <!DOCTYPE html>
+
+<div class="top-right links">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/dashboard') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">Login</a>
+            @endif
+        </div>
+    @endif
+</div>
+<style>
+
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-weight: 300;
+    }
+
+    body {
+        font-family: 'Source Sans Pro', sans-serif;
+        color: white;
+        font-weight: 300;
+    }
+
+    body ::-webkit-input-placeholder {
+        font-family: 'Source Sans Pro', sans-serif;
+        color: white;
+        font-weight: 300;
+    }
+
+    body ::-moz-placeholder {
+        font-family: 'Source Sans Pro', sans-serif;
+        color: white;
+        opacity: 1;
+        font-weight: 300;
+    }
+
+    body :-ms-input-placeholder {
+        font-family: 'Source Sans Pro', sans-serif;
+        color: white;
+        opacity: 1;
+        font-weight: 300;
+    }
+
+    .contenido1 {
+        position: absolute;
+        width: 100%;
+        height: 917px;
+        margin-top: -300px;
+        overflow: hidden;
+    }
+
+    .containerr {
+        max-width: 600px;
+        margin: 0 auto;
+        height: 1500px;
+    }
+
+    .burbujas {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+    }
+
+    .burbujas li {
+        position: absolute;
+        list-style: none;
+        display: block;
+        text-align: center;
+        color: white;
+        -webkit-border-radius: 50%;
+        -moz-border-radius: 50%;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        border: 1px solid rgba(255, 255, 255, .7);
+        background-color: rgba(255, 255, 255, 0.3);
+        bottom: -160px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, .5);
+        -webkit-animation: square 25s infinite;
+        -moz-animation: square 25s infinite;
+        animation: square 25s infinite;
+        -webkit-transition-timing-function: linear;
+        -moz-transition-timing-function: linear;
+        transition-timing-function: linear;
+    }
+
+    .burbujas li span {
+        position: absolute;
+        display: block;
+        -webkit-border-radius: 50%;
+        -moz-border-radius: 50%;
+        border-radius: 50%;
+        width: 90%;
+        height: 90%;
+        background-color: rgba(255, 255, 255, .15);
+    }
+
+    .burbujas li:nth-child(1) {
+        left: 10%;
+
+    }
+
+    .burbujas li:nth-child(2) {
+        left: 20%;
+        width: 80px;
+        height: 80px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, 0);
+        -webkit-animation-delay: 2s;
+        -moz-animation-delay: 2s;
+        animation-delay: 2s;
+        -webkit-animation-duration: 17s;
+        -moz-animation-duration: 17s;
+        animation-duration: 17s;
+    }
+
+    .burbujas li:nth-child(3) {
+        left: 25%;
+        -webkit-animation-delay: 4s;
+        -moz-animation-delay: 4s;
+        animation-delay: 4s;
+    }
+
+    .burbujas li:nth-child(4) {
+        left: 40%;
+        width: 60px;
+        height: 60px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, .2);
+        -webkit-animation-duration: 22s;
+        -moz-animation-duration: 22s;
+        animation-duration: 22s;
+        background-color: rgba(255, 255, 255, .25);
+    }
+
+    .burbujas li:nth-child(5) {
+        left: 70%;
+    }
+
+    .burbujas li:nth-child(6) {
+        left: 80%;
+        width: 130px;
+        height: 130px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, 0);
+        -webkit-animation-delay: 3s;
+        -moz-animation-delay: 3s;
+        animation-delay: 3s;
+        background-color: rgba(255, 255, 255, .22);
+    }
+
+    .burbujas li:nth-child(7) {
+        left: 32%;
+        width: 160px;
+        height: 160px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, 0);
+        -webkit-animation-delay: 7s;
+        -moz-animation-delay: 7s;
+        animation-delay: 7s;
+    }
+
+    .burbujas li:nth-child(8) {
+        left: 55%;
+        width: 20px;
+        height: 20px;
+        -webkit-animation-delay: 15s;
+        -moz-animation-delay: 15s;
+        animation-delay: 15s;
+        -webkit-animation-duration: 40s;
+        -moz-animation-duration: 40s;
+        animation-duration: 40s;
+    }
+
+    .burbujas li:nth-child(9) {
+        left: 65%;
+        width: 35px;
+        height: 35px;
+        -webkit-animation-delay: 2s;
+        -moz-animation-delay: 2s;
+        animation-delay: 2s;
+        -webkit-animation-duration: 40s;
+        -moz-animation-duration: 40s;
+        animation-duration: 40s;
+        background-color: rgba(255, 255, 255, .3);
+    }
+
+    .burbujas li:nth-child(10) {
+        left: 90%;
+        width: 160px;
+        height: 160px;
+        box-shadow: 0 0 20px 1px rgba(255, 255, 255, 0);
+        -webkit-animation-delay: 11s;
+        -moz-animation-delay: 11s;
+        animation-delay: 11s;
+    }
+
+    @-webkit-keyframes square {
+        0% {
+            -webkit-transform: translateY(0);
+            -moz-transform: translateY(0);
+            transform: translateY(0);
+        }
+        100% {
+            -webkit-transform: translateY(-700px) rotate(600deg);
+            -moz-transform: translateY(-700px) rotate(600deg);
+            transform: translateY(-700px) rotate(600deg);
+        }
+    }
+
+    @keyframes square {
+        0% {
+            -webkit-transform: translateY(0);
+            -moz-transform: translateY(0);
+            transform: translateY(0);
+        }
+        100% {
+            -webkit-transform: translateY(-700px) rotate(600deg);
+            -moz-transform: translateY(-700px) rotate(600deg);
+            transform: translateY(-700px) rotate(600deg);
+        }
+    }
+
+    .bg-animado-blue {
+        background-color: #0770ac;
+        background: -webkit-linear-gradient(24deg, #7be9fb, #0770ac, #23b9ff);
+        background: -moz-linear-gradient(24deg, #7be9fb, #0770ac, #23b9ff);
+        background: linear-gradient(24deg, #7be9fb, #0770ac, #23b9ff);
+        background-size: 600% 100%;
+        -webkit-animation: AnimationName 11s ease infinite;
+        -moz-animation: AnimationName 11s ease infinite;
+        animation: AnimationName 11s ease infinite;
+    }
+
+    @-webkit-keyframes AnimationName {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 48%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
+
+    @keyframes AnimationName {
+        0% {
+            background-position: 0% 50%
+        }
+        50% {
+            background-position: 100% 48%
+        }
+        100% {
+            background-position: 0% 50%
+        }
+    }
+</style>
+
+<div class="contenido1 bg-animado-blue">
+    <div class="containerr">
+    </div>
+    <ul class="burbujas">
+        <li><span>C++</span></li>
+        <li><span>C</span></li>
+        <li><span>Java</span></li>
+        <li><span>SQL</span></li>
+        <li><span>PHP</span></li>
+        <li><span>code</span></li>
+        <li><span>FCIH</span></li>
+        <li><span>Quizzes</span></li>
+        <li><span>solve</span></li>
+        <li><span>Courses</span></li>
+    </ul>
+</div>
+
+
 <html lang="{{ app()->getLocale() }}">
 
 <head>
@@ -46,24 +326,87 @@
         }
 
     </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Login</h4>
+            </div>
+            <!--------------login form-------------->
+            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <div class="modal-body">
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">Password</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-8 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Login
+                            </button>
+
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                Forgot Your Password?
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </form>
+            <!--------------end login form-------------->
+        </div>
+
+    </div>
+</div>
 <div class="container-fluid main-header-text">
     <div class="top-header-container">
         <div id="home" class="header-cell"
-             style="background-image:url({{asset('welcome/img/DD-Starry-Night-Sky-Scene-64536-Preview.jpg')}});height:667px;">
-            <div class="top-right links">
-                @if (Route::has('login'))
-                    <div class="top-right links">
-                        @if (Auth::check())
-                            <a href="{{ url('/dashboard') }}">Home</a>
-                        @else
-                            <a href="{{ url('/login') }}">Login</a>
-                        @endif
-                    </div>
-                @endif
-            </div>
+             style="height:667px;">
             <h1 style="font-family:'Rajdhani', sans-serif; letter-spacing: 2px;"><span
                         style="color: #0EBCF3;">FCI-H</span> Learning management System</h1>
             <p style="font-family:'Rajdhani', sans-serif; font-size: 20px;">Easy Communication Between Instructors and
@@ -84,6 +427,15 @@
                     <li class="active" role="presentation"><a href="#home">Home </a></li>
                     <li role="presentation"><a href="#features">Features </a></li>
                     <li role="presentation"><a href="#team">Team </a></li>
+                    <li role="presentation">
+                        @if (Route::has('login'))
+                            @if (Auth::check())
+                                <a  style="color:#0EBCF3;" href="{{ url('/dashboard') }}">Home</a>
+                            @else
+                                <button style="color:#0EBCF3; background-color:inherit; border:none;" type="button" data-toggle="modal" data-target="#myModal">Login</button>
+                            @endif
+                        @endif
+                    </li>
                 </ul>
             </div>
         </div>
